e1f7f3f8663debb46a3a222b5ef346676085a07e
ducviettiendoan <ducviettiendoan@gmail.com>
2023-11-13 14:49:35 -0500

    add jenkins job

---
+++ jenkins-job/src/main/java/com/sb/sb/privacy/SDObject.java
@@ -0,0 +1,29 @@
+package com.sb.sb.privacy;
+
+import java.util.*;
+import org.springframework.stereotype.Service;
+
+import java.io.*;
+
+@Service
+public class SDObject {
+    public static void main(String[] args) throws IOException {}
+    /** Read the object from Base64 string. */
+   public Object fromString( String s ) throws IOException , ClassNotFoundException {
+        byte [] data = Base64.getDecoder().decode( s );
+        ObjectInputStream ois = new ObjectInputStream( 
+                                        new ByteArrayInputStream(  data ) );
+        Object o  = ois.readObject();
+        ois.close();
+        return o;
+   }
+
+    /** Write the object to a Base64 string. */
+    public String toString( Serializable o ) throws IOException{
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream( baos );
+        oos.writeObject( o );
+        oos.close();
+        return Base64.getEncoder().encodeToString(baos.toByteArray()); 
+    }
+}
