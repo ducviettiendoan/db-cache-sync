6b85c98251d2044e7d17e2f996bf4b65c998d049
ducviettiendoan <ducviettiendoan@gmail.com>
2023-11-14 19:48:58 -0500

    fix sb

--- sb/src/main/java/com/sb/sb/SbApplication.java
+++ sb/src/main/java/com/sb/sb/SbApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.repository.support.Repositories;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -41,6 +42,7 @@ import java.util.stream.Collectors;
 
 @SpringBootApplication
 @EnableAsync
+@ComponentScan({"com", "com.sb.sb"})
 @RestController
 @RequestMapping("/")
 public class SbApplication{
